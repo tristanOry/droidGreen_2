e773f0963421f904e39e72b0fd56ce754c3d3e18
tristanORY <t.ory@laposte.net>
2020-04-15 14:25:13 +0200

    Login fonctionnel Début de travail sur l'api catalogue : liste des produits OK

--- vg_api/catalog.php
+++ vg_api/catalog.php
@@ -3,21 +3,23 @@
 require_once 'includes/DbCatalog.php';
 $response = array();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	if(isset $_POST['cat']){
+	if(isset ($_POST['cat'])){
 		$db=new dbCatalog();
 		// Si une requête post a été envoyée et qu'il s'agit bien des requêtes catalogue
 		switch($_POST['cat']){
 			case 'listecat':
-				$response['result']=$db->listeCat();
-				$response['error']=false;
+				$response=$db->listeCat();				
 				break;
 			case 'listesouscat':
-				$response['result']=$db->listeSousCat($_POST['parent']);
+				$response=$db->listeSousCat($_POST['parent']);
 				$response['error']=false;
 				break;
 			case 'produits':
-				$response['result']=$db->listeProd($_POST['filtre'],$_POST['tri']);
-				$response['error']=false;
+				$filtre=(isset($_POST['filtre']))?$_POST['filtre']:"";
+				$cle=(isset($_POST['cle']))?$_POST['cle']:"";
+				$val_filtre=(isset($_POST['vfiltre']))?$_POST['vfiltre']:"";
+				$ordre=(isset($_POST['ordre']))?$_POST['ordre']:"";
+				$response['result']=$db->listeProd($filtre,$val_filtre,$cle,$ordre);
 				break;
 			default:
 				$response['error']=true;
@@ -31,5 +33,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$response['error']=true;
 	$response['message']="Mauvaise méthode d'appel de l'API";
 }
-
+//var_dump($_POST);
 echo json_encode($response);
--- vg_api/includes/DbCatalog.php
+++ vg_api/includes/DbCatalog.php
@@ -28,7 +28,7 @@ class DbCatalog {
 		return $this->result;
 	}
 	
-	function listProd($filtre="", $valFiltre="", $cle="", $ordre=""){
+	function listeProd($filtre="", $valFiltre="", $cle="", $ordre=""){
 		$i=0;
 		$req="SELECT * FROM `produit`";
 		// Si $filtre est défini
@@ -54,7 +54,7 @@ class DbCatalog {
 			}
 		} 
 		// Si il y a une demande de tri et que les paramètres de filtre sont OK
-		if(!(empty($cle) || isset($this->result['error'])) {
+		if(!(empty($cle) || isset($this->result['error']))) {
 			$tmp=strtolower($cle);
 			// Si une valeur a été passée pour le filtre, alors on a un second paramètre, sinon c'est le premier
 			switch($tmp){
@@ -74,10 +74,15 @@ class DbCatalog {
 			if($ordre<0) $req.=" DESC";
 		}
 		// Si toujours pas d'erreur dans la requête
-		if(!isset($this->result['error']){
+		if(!isset($this->result['error'])){
 			$stmt=$this->con->prepare($req);
 			if($i>0) $stmt->bind_param("s",$p1);
-			$this->result=$stmt->get_result()->fetch_assoc();
+			$stmt->execute();
+			$r=$stmt->get_result();
+			// On lit tous les enregistrements (malheureusement le dernier est null)
+			while($this->result[]=$r->fetch_assoc());
+			//donc on le supprime
+			array_pop($this->result);
 			$this->result['error']=false;
 		}
 		return $this->result;
